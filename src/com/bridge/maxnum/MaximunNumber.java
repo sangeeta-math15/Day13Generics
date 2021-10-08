f7fb5432e07585ec6d79b77a4747cff2b67fdc3a
sngtmth <sngtmth@gmail.com>
2021-10-08 21:44:46 +0530

    Added: Three Floating Numbers to find maximum number

--- src/com/bridge/maxnum/MaximunNumber.java
+++ src/com/bridge/maxnum/MaximunNumber.java
@@ -8,11 +8,12 @@ package com.bridge.maxnum;
  * @param <T>
  ************************************/
 
-public class MaximunNumber {
+public class MaximunNumber<T> {
+
 	public static <T> void printMax(T a, T b, T c, T max) {
 		System.out.println("First Number=>" + a);
 		System.out.println("Sec Number=>" + b);
-		System.out.println("Three Number=>" + c);
+		System.out.println("Thrird Number=>" + c);
 		System.out.println("Maximum Value is=>" + max);
 
 	}
@@ -31,14 +32,32 @@ public class MaximunNumber {
 
 	}
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Maximum Problem using Generics");
-		System.out.println();
+	/*
+	 * UC2-Find max number among three double value
+	 */
+	public static Double testMaxDouble(Double a, Double b, Double c) {
+		Double max = a;// assume x is initially largest
+		if (b.compareTo(max) > 0)
+			max = b;
+		if (c.compareTo(max) > 0)
+			max = c;
+		printMax(a, b, c, max);
+		return max;
+
+	}
+
+	public static <T> void main(String[] args) {
 		System.out.println(testMaxInteger(90, 20, 10));
 		System.out.println();
-		System.out.println(testMaxInteger(27, 94, 19));
+		System.out.println(testMaxInteger(94, 27, 19));
 		System.out.println();
-		System.out.println(testMaxInteger(8, 16, 45));
-
+		System.out.println(testMaxInteger(8, 45, 16));
+		System.out.println();
+		System.out.println(testMaxDouble(90.5, 20.6, 10.7));
+		System.out.println();
+		System.out.println(testMaxDouble(94.3, 27.4, 19.2));
+		System.out.println();
+		System.out.println(testMaxDouble(8.9, 45.7, 16.3));
+		System.out.println();
 	}
 }
