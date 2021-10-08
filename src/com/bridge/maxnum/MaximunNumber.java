f5351241cefe469471118d0d02a49d9ac215d0fa
sngtmth <sngtmth@gmail.com>
2021-10-08 21:09:43 +0530

    Added:Welcome message

---
+++ src/com/bridge/maxnum/MaximunNumber.java
@@ -0,0 +1,15 @@
+package com.bridge.maxnum;
+
+/***********************************
+ * Given three integer to find maximum number using generic class and method
+ * 
+ * @author Sangeeta Math
+ *
+ * @param <T>
+ ************************************/
+
+public class MaximunNumber {
+	public static void main(String args[]) {
+		System.out.println("Welcome to Maximum Problem using Generics");
+	}
+}
